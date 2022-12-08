a47e75776046c7433ce425ad8f3cdd59d4cdfb7f
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-08 23:32:56 +0300

    add tasks for Array.entries()

---
+++ mdn/JavaScript/Reference/Global_Objects/Array/entries/tasks_descriptions/1_task.js
@@ -0,0 +1,12 @@
+/**
+ * #Task 1:
+ * [] - Не запуская код, что в качестве результата вернет arr.entries()
+ * [] - Поиграйтесь со значениями в eArr какие методы дает IDE, попробуйте их подергать.
+ */
+const arr = ['a', 'b', 'c'];
+const eArr = arr.entries();
+
+//TODO code here
+
+// # //TODO read about Iterators and generators https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators
+// # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/entries
