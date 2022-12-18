69acc9906f8086e2f303b15c10f8361d24e48692
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-18 20:37:11 +0300

    add array.fill()

---
+++ mdn/JavaScript/Reference/Global_Objects/Array/fill/tasks_descriptions/2_task.js
@@ -0,0 +1,8 @@
+/**
+ * #Task 2: Не запуская код:
+ * что будет содержать массив arr? после 5 строки, и почему?
+ */
+const arr = Array(3).fill({})
+arr[0].hi = "hi";
+
+// # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/fill#%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D1%8B
