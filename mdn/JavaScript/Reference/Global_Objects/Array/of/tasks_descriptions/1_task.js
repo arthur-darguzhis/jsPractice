6a02043b017c61ae9f753703f60e14f4de99b4d8
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-20 00:07:27 +0300

    improve description and add arr.of()

---
+++ mdn/JavaScript/Reference/Global_Objects/Array/of/tasks_descriptions/1_task.js
@@ -0,0 +1,10 @@
+/**
+ * #Task 1: Не запуская кода скажите что вернут каждая из строк?
+ * Проверьте себя.
+ */
+
+Array.of(1);
+Array.of(1, 2, 3);
+Array.of(undefined);
+
+// # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/of#%D0%BE%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5
--- mdn/tasksThatAreAlreadyInAnki.js
+++ mdn/tasksThatAreAlreadyInAnki.js
@@ -1,4 +1,6 @@
 const tasksInAnki = [
+  'jetbrains://web-storm/navigate/reference?project=jsPractice&path=mdn/JavaScript/Reference/Global_Objects/Array/of/1_task.js',
+  'jetbrains://web-storm/navigate/reference?project=jsPractice&path=mdn/JavaScript/Reference/Global_Objects/Array/of/2_task.js',
   'jetbrains://web-storm/navigate/reference?project=jsPractice&path=mdn/JavaScript/Reference/Global_Objects/Array/map/1_task.js',
   'jetbrains://web-storm/navigate/reference?project=jsPractice&path=mdn/JavaScript/Reference/Global_Objects/Array/map/2_task.js',
   'jetbrains://web-storm/navigate/reference?project=jsPractice&path=mdn/JavaScript/Reference/Global_Objects/Array/map/3_task.js',
