dcc9a185a1ef7ef20d7c30706abd8df8a50c2a34
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-19 11:40:39 +0300

    improve description and add arr.find()

---
+++ mdn/JavaScript/Reference/Global_Objects/Array/find/tasks_descriptions/1_task.js
@@ -0,0 +1,9 @@
+/**
+ * #Task 1: Найдите первый элемент который больше 10
+ */
+
+const array1 = [5, 12, 8, 130, 44];
+const found = array1. //TODO write code here;
+console.log(found);
+
+// # На случаи затруднений: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find#try_it
---
+++ mdn/JavaScript/Reference/Global_Objects/Array/find/tasks_descriptions/2_task.js
@@ -0,0 +1,14 @@
+/**
+ * #Task 2: Найдите объект где в свойстве "name" значение "cherries"
+ */
+
+const inventory = [
+  { name: "apples", quantity: 2 },
+  { name: "bananas", quantity: 0 },
+  { name: "cherries", quantity: 5 },
+];
+
+console.log(inventory. /** //TODO write code here */);
+// { name: 'cherries', quantity: 5 }
+
+// # На случаи затруднений: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find#try_it
