7cdbcdffa417a49ab07552668007bf5b73fba38c
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-19 12:08:57 +0300

    improve description and add arr.findIndex()

---
+++ mdn/JavaScript/Reference/Global_Objects/Array/findIndex/tasks_descriptions/2_task.js
@@ -0,0 +1,14 @@
+/**
+ * #Task 2: Найдите индекс элемента содержащего объект где в свойстве "name" значение "cherries"
+ */
+
+const inventory = [
+  { name: "apples", quantity: 2 },
+  { name: "bananas", quantity: 0 },
+  { name: "cherries", quantity: 5 },
+];
+
+console.log(inventory. /** //TODO write code here */); //2
+
+
+// # На случаи затруднений: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find#try_it
