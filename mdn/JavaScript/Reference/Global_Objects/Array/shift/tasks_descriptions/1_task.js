672c929d830c43a1bb860e38f9e2db89e015cd34
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-20 15:09:49 +0300

    improve description and add arr.shift()

---
+++ mdn/JavaScript/Reference/Global_Objects/Array/shift/tasks_descriptions/1_task.js
@@ -0,0 +1,10 @@
+/**
+ * #Task 1: Получите первый элемент массива.
+ * Как измениться сам массив?
+ */
+
+const array = [1, 2, 3];
+const firstElement = //TODO write code here
+console.log(array);
+
+// # На случаи затруднений: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
