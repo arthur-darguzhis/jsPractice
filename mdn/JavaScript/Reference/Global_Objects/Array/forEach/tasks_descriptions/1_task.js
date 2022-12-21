35e225465452bd9fd101a29c3094f8ab61a3c4f1
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-21 22:21:18 +0300

    improve description

--- mdn/JavaScript/Reference/Global_Objects/Array/forEach/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/forEach/tasks_descriptions/1_task.js
@@ -10,5 +10,6 @@ const copy = []
 // }
 
 //TODO write code here
+console.log(copy)
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach#%D0%BF%D0%B5%D1%87%D0%B0%D1%82%D1%8C_%D1%81%D0%BE%D0%B4%D0%B5%D1%80%D0%B6%D0%B8%D0%BC%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D0%B0
--- mdn/JavaScript/Reference/Global_Objects/Array/from/tasks_descriptions/3_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/from/tasks_descriptions/3_task.js
@@ -2,9 +2,9 @@
  * #Task 3: Сделайте массив из Set
  */
 
-const s = new Set(['foo', window]);
+const s = new Set(['foo', globalThis]);
 //TODO write code here
 
-// ['foo', window]
+// ['foo', globalThis]
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/from#%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2_%D0%B8%D0%B7_set
