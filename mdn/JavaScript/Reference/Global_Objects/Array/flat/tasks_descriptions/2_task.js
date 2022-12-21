b48ed307e9f54a9db371b90455e795ba7b83ae1f
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-21 20:42:16 +0300

    improve description

--- mdn/JavaScript/Reference/Global_Objects/Array/flat/tasks_descriptions/2_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/flat/tasks_descriptions/2_task.js
@@ -1,10 +1,8 @@
 /**
- * #Task 2: рекурсивно поднимает вложенность массивов,
- * что бы получить следующий результат [1, 2, 3, 4, [5, 6]]
+ * #Task 2: преобразуйте массив [1, 2, [3, 4, [5, 6]]] в [1, 2, 3, 4, [5, 6]]
  */
 
-const arr1 = [1, 2, [3, 4, [5, 6]]];
+const arr = [1, 2, [3, 4, [5, 6]]];
 console.log( /** TODO write code here */);
-// expected output: [1, 2, 3, 4, [5, 6]]
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/flat#%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D1%8B
--- mdn/JavaScript/Reference/Global_Objects/Array/flat/tasks_descriptions/4_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/flat/tasks_descriptions/4_task.js
@@ -1,14 +1,14 @@
 /**
- * #Task 4: рекурсивно поднимает вложенность массивов,
- * что бы получить следующий результат [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ * #Task 4: Установите нужное значение переменной depth (не число) что бы
+ * преобразовать массив [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]] в [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
  */
-const depth = ; /** TODO write code here */
-if (typeof depth === "number") {
+
+const depth =; /** TODO write code here */
+if (typeof depth < 100) {
   throw new Error("value for \"deep\" variable must not be number");
 }
 
-const arr1 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
-console.log(arr1.flat(depth));
-// expected output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const arr = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+console.log(arr.flat(depth));
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/flat#%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D1%8B
