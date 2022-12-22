39f8aa2f26a921c289ff21ee1f662deae75e6534
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-22 11:26:44 +0300

    improve description

--- mdn/JavaScript/Reference/Global_Objects/Array/join/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/join/tasks_descriptions/1_task.js
@@ -1,8 +1,6 @@
-/**
- * #Task 1: Объедините элементы массива в строку разделяя их '=>'
- */
-const elements = ['Fire', 'Air', 'Water'];
+import * as assert from "assert";
 
-//TODO write code here
+const elements = ["Fire", "Air", "Water"];
+assert.strictEqual(elements, "Fire=>Air=>Water")
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/join#%D1%81%D0%BE%D0%B5%D0%B4%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5_%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D0%B0_%D1%87%D0%B5%D1%82%D1%8B%D1%80%D1%8C%D0%BC%D1%8F_%D1%80%D0%B0%D0%B7%D0%BB%D0%B8%D1%87%D0%BD%D1%8B%D0%BC%D0%B8_%D1%81%D0%BF%D0%BE%D1%81%D0%BE%D0%B1%D0%B0%D0%BC%D0%B8
