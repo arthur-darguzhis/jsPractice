04d7973dc5bf3f1b3f265521e3432573ceaaef71
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-22 14:12:29 +0300

    improve description

--- mdn/JavaScript/Reference/Global_Objects/Array/lastIndexOf/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/lastIndexOf/tasks_descriptions/1_task.js
@@ -1,8 +1,13 @@
+import assert from "assert";
+
 /**
- * #Task 1: Верните последний возможный индекс элемента 4
+ * Ищем справа на лево, элемент со значением 4 и возвращаем его индекс
+ *                          👇
+ * [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+ *  <- <- <- <- <- <- <- <- <- <-
  */
 
-[1, 2, 3, 4, 5, 1, 2, 3, 4, 5] //TODO write code here
-//true
+const lastIndex = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]; //TODO write code here
 
+assert.equal(lastIndex, 8);
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf#%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D0%B2%D0%BD%D1%8B%D0%B9_%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80
--- mdn/JavaScript/Reference/Global_Objects/Array/lastIndexOf/tasks_descriptions/4_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/lastIndexOf/tasks_descriptions/4_task.js
@@ -1,21 +1,16 @@
+import assert from "assert";
+
 /**
- * #Task 4: Проверьте утверждение:
- *
- * lastIndexOf() специально сделан общим.
- * Он не требует, чтобы this являлся массивом,
- * так что он может быть применён к другим типам объектов
- * (например, к массивоподобным объектам). Такие, как объект
- *
- *
- * Напишите код использующий метода lastIndexOf() скоплении аргументов функции "Остаточном параметре" ...args
- * а не args https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Functions/arguments
+ * Организуйте поиск в направлении справа налево
+ * [1, 2, 3, 1, 2, 3, 1, 2, 3]
+ *  <- <- <- <- <- <- <- <- <-
+ * Верните индекс первого встретившегося элемента со значением 1
  */
-
 const fn = (...args) => {
-  args. //TODO write code here
-}
+  return; //TODO write code here
+};
 
-fn(1,2,3,4,5,6,7,'a')
+assert.equal(fn(1, 2, 3, 1, 2, 3, 1, 2, 3), 6);
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf#%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D0%B2%D0%BD%D1%8B%D0%B9_%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80
 
