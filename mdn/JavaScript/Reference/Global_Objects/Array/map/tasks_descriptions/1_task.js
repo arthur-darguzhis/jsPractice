7a8b8ac9775b0eac0e1ddf42bd924feb8eba3512
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-12 00:34:14 +0300

    improve task description

--- mdn/JavaScript/Reference/Global_Objects/Array/map/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/map/tasks_descriptions/1_task.js
@@ -11,3 +11,4 @@ const roots =  //TODO write code here
 assert.deepEqual(roots, [1, 2, 3])
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/map#%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80_%D0%BE%D1%82%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D0%B0_%D1%87%D0%B8%D1%81%D0%B5%D0%BB_%D0%BD%D0%B0_%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2_%D0%BA%D0%B2%D0%B0%D0%B4%D1%80%D0%B0%D1%82%D0%BD%D1%8B%D1%85_%D0%BA%D0%BE%D1%80%D0%BD%D0%B5%D0%B9
+
