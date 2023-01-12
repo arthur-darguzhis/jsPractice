c07e03fe521cdf6826ba5c52e101152133626009
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-12 22:10:27 +0300

    improve task description

--- mdn/JavaScript/Reference/Global_Objects/Array/reduce/tasks_descriptions/3_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/reduce/tasks_descriptions/3_task.js
@@ -1,14 +1,28 @@
 /**
- * #Task 3: склейте массив содержащий все книги из объектов
+ * #Task 3: создать массив, каждый элемент которого название книги из списка объектов
  */
+import assert from "assert";
 
 const friends = [
-  { name: "Anna", books: ["Bible", "Harry Potter"], age: 21 },
-  { name: "Bob", books: ["War and peace", "Romeo and Juliet"], age: 26 },
-  { name: "Alice", books: ["The Lord of the Rings", "The Shining"], age: 18 }
-]
+  {
+    name: "Anna",
+    books: ["Bible", "Harry Potter"],
+    age: 21
+  },
+  {
+    name: "Bob",
+    books: ["War and peace", "Romeo and Juliet"],
+    age: 26
+  },
+  {
+    name: "Alice",
+    books: ["The Lord of the Rings", "The Shining"],
+    age: 18
+  }
+];
 
-var allbooks = //TODO write code here
-// ["Romeo and Juliet", "The Lord of the Rings", "The Shining"]
+const allbooks =  //TODO write code here
 
+
+assert.deepEqual(allbooks, ["Bible", "Harry Potter", "War and peace", "Romeo and Juliet", "The Lord of the Rings", "The Shining"]);
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce#%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80_%D1%81%D0%BA%D0%BB%D0%B5%D0%B8%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5_%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D0%BE%D0%B2_%D1%81%D0%BE%D0%B4%D0%B5%D1%80%D0%B6%D0%B0%D1%89%D0%B8%D1%85%D1%81%D1%8F_%D0%B2_%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%B0%D1%85_%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D0%B0_%D1%81_%D0%B8%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5%D0%BC_%D0%BE%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%B0_%D1%80%D0%B0%D1%81%D1%88%D0%B8%D1%80%D0%B5%D0%BD%D0%B8%D1%8F_%D0%B8_initialvalue
