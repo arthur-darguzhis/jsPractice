86cd133d329b691eb65caf296de6fa32911e10ce
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-12 21:53:46 +0300

    improve task description

--- mdn/JavaScript/Reference/Global_Objects/Array/push/tasks_descriptions/2_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/push/tasks_descriptions/2_task.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 /**
  * #Task 2: Влейте значения из additionalSports в sports посредством push
  * что бы каждый элемент был добавлен отдельно, а не добавлен массив целиком
@@ -6,4 +7,8 @@
 const sports = ['футбол', 'бейсбол'];
 const additionalSports = ['тенис', 'бокс', 'прыжки в длину']
 
+//TODO write code here
+
+
+assert.deepEqual(sports, ['футбол', 'бейсбол', 'тенис', 'бокс', 'прыжки в длину'])
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/push#%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80_%D0%B4%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D0%BE%D0%B2_%D0%B2_%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2
