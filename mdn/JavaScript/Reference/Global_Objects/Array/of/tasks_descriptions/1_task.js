0d6356d8c1da6399da130608015ba624368777bb
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-12 21:46:23 +0300

    improve task description

--- mdn/JavaScript/Reference/Global_Objects/Array/of/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/of/tasks_descriptions/1_task.js
@@ -5,6 +5,6 @@
 
 Array.of(1);
 Array.of(1, 2, 3);
-Array.of(undefined);
+Array.of(undefined, 'asdf');
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/of#%D0%BE%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5
