b732f7e3fedb3214e70318abbfc20a0206ff2772
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-16 15:33:10 +0300

    improve task description

--- mdn/JavaScript/Reference/Global_Objects/Array/reduceRight/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/reduceRight/tasks_descriptions/1_task.js
@@ -5,11 +5,14 @@
  * Option1 с initialValue
  * Option2 без initialValue
  */
+import assert from "assert";
 
 const array1 = [1, 2, 3, 4];
-//TODO Option1 write code here
+const res1; //TODO Option1 write code here with "initialValue"
 
 const array2 = [1, 2, 3, 4];
-//TODO Option2 write code here
+const res2; //TODO Option2 write code here without "initialValue"
 
+assert.equal(res1, 10)
+assert.equal(res2, 10)
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce#%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D0%B2%D0%BD%D1%8B%D0%B9_%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80
