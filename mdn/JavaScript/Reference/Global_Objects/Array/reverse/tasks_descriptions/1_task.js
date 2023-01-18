94198c21d2268fb5dec727b485d47959e9c51c99
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-18 08:47:11 +0300

    improve task description

--- mdn/JavaScript/Reference/Global_Objects/Array/reverse/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/reverse/tasks_descriptions/1_task.js
@@ -1,9 +1,10 @@
+import assert from "assert";
 /**
  * #Task 1: переверните массив
  */
 
-const myArray = ['one', 'two', 'three'];
+const myArray = ["one", "two", "three"];
 //TODO write code here
-// ['three', 'two', 'one']
+assert.deepEqual(myArray, ["three", "two", "one"]);
 
 // # На случаи затруднений: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse#try_it
