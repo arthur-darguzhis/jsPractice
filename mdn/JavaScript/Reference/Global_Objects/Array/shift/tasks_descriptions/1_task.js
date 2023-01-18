1162ccbbb8f3b91bba055738d56782e4f3aa9699
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-18 08:51:53 +0300

    improve task description

--- mdn/JavaScript/Reference/Global_Objects/Array/shift/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/shift/tasks_descriptions/1_task.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 /**
  * #Task 1: Получите первый элемент массива.
  * Как измениться сам массив?
@@ -6,5 +7,7 @@
 const array = [1, 2, 3];
 const firstElement = //TODO write code here
 console.log(array);
+assert.deepEqual(array, [2, 3])
+assert.equal(firstElement, 1)
 
 // # На случаи затруднений: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
