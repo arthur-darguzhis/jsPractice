103981f74ab09a50a04deaa7b9c1952baa9554be
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-19 14:28:39 +0300

    improve task description

--- mdn/JavaScript/Reference/Global_Objects/Array/sort/tasks_descriptions/2_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/sort/tasks_descriptions/2_task.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 /**
  * #Task 2: Отсортируйте объекты по свойству name
  */
@@ -11,4 +12,14 @@ const items = [
   { name: 'Zeros', value: 37 }
 ];
 
+//TODO write code here
+
+assert.deepEqual(items, [
+  { name: 'And', value: 45 },
+  { name: 'Edward', value: 21 },
+  { name: 'Magnetic' },
+  { name: 'Sharpe', value: 37 },
+  { name: 'The', value: -12 },
+  { name: 'Zeros', value: 37 }
+]);
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#%D0%BE%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5
