de9d3b18722847cbbff18639a070047eb0ea443d
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-23 01:03:46 +0300

    improve description

--- mdn/JavaScript/Reference/Global_Objects/Array/splice/tasks_descriptions/3_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/splice/tasks_descriptions/3_task.js
@@ -1,13 +1,13 @@
+import assert from "assert";
 /**
  * #Task 3: С помощью splice удалите элемент "drum" и вставьте на его место "trumpet"
  * Какой результат будет в 7 строке и почему?
  */
 
-const myFish = ['angel', 'clown', 'drum', 'sturgeon'];
-const removed = myFish. //TODO write code here
-  ["angel", "clown", "trumpet", "sturgeon"]
+const myFish = ["angel", "clown", "drum", "sturgeon"];
+const removed = ;//TODO write code here
 
-// myFish равен ["angel", "clown", "trumpet", "sturgeon"]
-// removed равен ["drum"]
+assert.deepEqual(removed,  ["drum"]);
+assert.deepEqual(myFish,  ["angel", "clown", "trumpet", "sturgeon"]);
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/splice#%D1%83%D0%B4%D0%B0%D0%BB%D1%8F%D0%B5%D1%82_1_%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82_%D0%BF%D0%BE_%D0%B8%D0%BD%D0%B4%D0%B5%D0%BA%D1%81%D1%83_2_%D0%B8_%D0%B2%D1%81%D1%82%D0%B0%D0%B2%D0%BB%D1%8F%D0%B5%D1%82_trumpet
