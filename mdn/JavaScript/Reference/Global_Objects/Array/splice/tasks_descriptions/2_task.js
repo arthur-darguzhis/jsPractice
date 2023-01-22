9a796cc1e7ce039eeef81ac33cf4bee24b8efbf2
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-23 00:59:21 +0300

    improve description

--- mdn/JavaScript/Reference/Global_Objects/Array/splice/tasks_descriptions/2_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/splice/tasks_descriptions/2_task.js
@@ -1,12 +1,13 @@
+import assert from "assert";
 /**
  * #Task 2: С помощью splice удалите элемент "mandarin"
  * Какой результат будет в 7 строке и почему?
  */
 
-const myFish = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
-const removed = myFish. //TODO write code here
+const myFish = ["angel", "clown", "drum", "mandarin", "sturgeon"];
+const removed =  //TODO write code here
 
-// removed равен ["mandarin"]
-// myFish равен ["angel", "clown", "drum", "sturgeon"]
+assert.deepEqual(removed, ["mandarin"]);
+assert.deepEqual(myFish, ["angel", "clown", "drum", "sturgeon"]);
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/splice#%D1%83%D0%B4%D0%B0%D0%BB%D1%8F%D0%B5%D1%82_1_%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82_%D0%BF%D0%BE_%D0%B8%D0%BD%D0%B4%D0%B5%D0%BA%D1%81%D1%83_3
