f4bff4285d3392d8e1c333224dd422410a71e7ba
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-23 10:45:27 +0300

    improve description

--- mdn/JavaScript/Reference/Global_Objects/Array/unshift/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/unshift/tasks_descriptions/1_task.js
@@ -1,7 +1,11 @@
+import assert from "assert";
 /**
  * #Task 1: Добавьте в начало масива значения -10 -5
  */
-const array1 = [1, 2, 3];
-array1. //TODO write code here
 
+const array1 = [1, 2, 3];
+const arrLength = array1; //TODO write code here
+
+assert.deepEqual(array1, [-10, -5, 1, 2, 3]);
+assert.deepEqual(arrLength, 5);
 // # На случаи затруднений: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift#try_it
