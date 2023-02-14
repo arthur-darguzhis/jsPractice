3f6a82c4e2f7fe585e7f0f0edccfa4c5a434a29b
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-15 01:58:30 +0300

    it-k add tasks

--- mdn/JavaScript/Reference/Global_Objects/Array/findIndex/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/findIndex/tasks_descriptions/1_task.js
@@ -1,10 +1,10 @@
+import assert from "assert";
 /**
  * #Task 1: Найдите индекс первый элемент который больше 10
- */
+ * */
 
 const array1 = [5, 12, 8, 130, 44];
-const found = array1. //TODO write code here;
-console.log(found); //1
+const found = array1; //TODO write code here;
+assert.equal(found, 1);
 
 // # На случаи затруднений: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex#try_it
-
