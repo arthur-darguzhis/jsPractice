36e902b7fb24ea1eb8e54c28b17c5ef817bf5e7e
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-15 02:05:43 +0300

    it-k add tasks

--- mdn/JavaScript/Reference/Global_Objects/Array/filter/tasks_descriptions/4_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/filter/tasks_descriptions/4_task.js
@@ -1,7 +1,7 @@
+import assert from "assert";
 /**
  * #Task 4:
- * Реализуйте функцию filterItems с двумя параметрами arr (массив) и query (строка)
- * Элементы массива должны фильтроваться на основе критериев поиска query (14-15) строки
+ * Реализуйте фильтрацию по вхождению подстроки в строку
  */
 
 const fruits = ['apple', 'banana', 'grapes', 'mango', 'orange'];
@@ -10,7 +10,7 @@ const filterItems = (arr, query) => {
   return arr.filter( /** //TODO write code here */)
 }
 
-console.log(filterItems(fruits, 'ap')); // ['apple', 'grapes']
-console.log(filterItems(fruits, 'an')); // ['banana', 'mango', 'orange']
+assert.deepEqual(['apple', 'grapes'], filterItems(fruits, 'ap'))
+assert.deepEqual(['banana', 'mango', 'orange'], filterItems(fruits, 'an'))
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#%D0%BF%D0%BE%D0%B8%D1%81%D0%BA_%D0%B2_%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D0%B5
