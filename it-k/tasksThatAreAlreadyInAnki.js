22a40c2662c22c8a282776f798295ab49919baa3
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-21 00:35:07 +0300

    add promise tasks

--- it-k/tasksThatAreAlreadyInAnki.js
+++ it-k/tasksThatAreAlreadyInAnki.js
@@ -1,4 +1,12 @@
 const tasksInAnki = [
+  'jetbrains://web-storm/navigate/reference?project=jsPractice&path=/Users/arthur/WebstormProjects/jsPractice/it-k/10 promises/promise10.js',
+  'jetbrains://web-storm/navigate/reference?project=jsPractice&path=/Users/arthur/WebstormProjects/jsPractice/it-k/10 promises/promise11.js',
+  'jetbrains://web-storm/navigate/reference?project=jsPractice&path=/Users/arthur/WebstormProjects/jsPractice/it-k/10 promises/promise12.js',
+  'jetbrains://web-storm/navigate/reference?project=jsPractice&path=/Users/arthur/WebstormProjects/jsPractice/it-k/10 promises/promise5.js',
+  'jetbrains://web-storm/navigate/reference?project=jsPractice&path=/Users/arthur/WebstormProjects/jsPractice/it-k/10 promises/promise6.js',
+  'jetbrains://web-storm/navigate/reference?project=jsPractice&path=/Users/arthur/WebstormProjects/jsPractice/it-k/10 promises/promise7.js',
+  'jetbrains://web-storm/navigate/reference?project=jsPractice&path=/Users/arthur/WebstormProjects/jsPractice/it-k/10 promises/promise8.js',
+  'jetbrains://web-storm/navigate/reference?project=jsPractice&path=/Users/arthur/WebstormProjects/jsPractice/it-k/10 promises/promise9.js',
   'jetbrains://web-storm/navigate/reference?project=jsPractice&path=/Users/arthur/WebstormProjects/jsPractice/it-k/10 promises/promise1.js',
   'jetbrains://web-storm/navigate/reference?project=jsPractice&path=/Users/arthur/WebstormProjects/jsPractice/it-k/10 promises/promise2.js',
   'jetbrains://web-storm/navigate/reference?project=jsPractice&path=/Users/arthur/WebstormProjects/jsPractice/it-k/10 promises/promise3.js',
