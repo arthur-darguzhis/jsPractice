4b808fe82ef7d831308145f7d8d7d1d6b9bc5681
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-22 18:39:39 +0300

    add promise tasks

--- mdn/JavaScript/Reference/Global_Objects/Array/filter/tasks_descriptions/3_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/filter/tasks_descriptions/3_task.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 /**
  * #Task 3: имплементируйте функцию isPrime что бы отфильтровать все простые числа в массиве
  * простое число - это положительное число которое делится без остатка только на 1 и на само себя.
@@ -9,6 +10,6 @@ function isPrime(num) {
   //TODO write code here
 }
 
-console.log(array.filter( /** code here */ )); // [2, 3, 5, 7, 11, 13]
+assert.deepEqual(array.filter(isPrime), [2, 3, 5, 7, 11, 13])
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#%D0%BD%D0%B0%D0%B9%D1%82%D0%B8_%D0%B2%D1%81%D0%B5_%D0%BF%D1%80%D0%BE%D1%81%D1%82%D1%8B%D0%B5_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D0%B5
