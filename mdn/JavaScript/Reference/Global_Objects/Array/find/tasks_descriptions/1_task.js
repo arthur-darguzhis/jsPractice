a88b263e5bafa01bf1b7fb99a83077cc0786b064
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-22 17:03:31 +0300

    add promise tasks

--- mdn/JavaScript/Reference/Global_Objects/Array/find/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/find/tasks_descriptions/1_task.js
@@ -1,9 +1,9 @@
+import assert from "assert";
 /**
  * #Task 1: Найдите первый элемент который больше 10
  */
 
 const array1 = [5, 12, 8, 130, 44];
-const found = array1. //TODO write code here;
-console.log(found); //12
-
+const found = array1; //TODO write code here;
+assert.equal(found, 12);
 // # На случаи затруднений: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find#try_it
