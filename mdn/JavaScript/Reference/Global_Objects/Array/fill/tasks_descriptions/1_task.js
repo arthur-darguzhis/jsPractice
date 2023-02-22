710192bf7483fd197ee187d775edb3d8872c8637
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-22 17:00:26 +0300

    add promise tasks

--- mdn/JavaScript/Reference/Global_Objects/Array/fill/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/fill/tasks_descriptions/1_task.js
@@ -1,7 +1,9 @@
+import assert from "assert";
 /**
  * #Task 1: используя метод fill установите "4" как значение каждого элемента массива.
  */
 
-[1, 2, 3] //TODO code here
+const result = [1, 2, 3] //TODO code here
 
+assert.deepEqual(result, [4, 4, 4])
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/fill#%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D1%8B
