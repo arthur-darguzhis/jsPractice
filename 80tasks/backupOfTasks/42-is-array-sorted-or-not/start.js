4f5eccf58054f52878cbef3c854fc628578b4ac5
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-22 16:13:48 +0300

    add promise tasks

--- 80tasks/backupOfTasks/42-is-array-sorted-or-not/start.js
+++ 80tasks/backupOfTasks/42-is-array-sorted-or-not/start.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 /** ЗАДАЧА 42 - Проверка сортировки массива
  *
  * 1. Создайте функцию "arraySortInfo" с одним параметром - "inputArray"
@@ -16,10 +17,10 @@ const b = [4, 10, 14, 25, 25, 50]
 const c = [150, 132, 80, 40]
 const d = [15, 26, 10, 23, 85]
 
-console.log(arraySortInfo(a)) // Некоторые элементы не являются числами
-console.log(arraySortInfo(b)) // Массив отсортирован по возрастанию
-console.log(arraySortInfo(c)) // Массив отсортирован по убыванию
-console.log(arraySortInfo(d)) // Массив не отсортирован
+assert.equal(arraySortInfo(a), "Некоторые элементы не являются числами")
+assert.equal(arraySortInfo(b), "Массив отсортирован по возрастанию")
+assert.equal(arraySortInfo(c), "Массив отсортирован по убыванию")
+assert.equal(arraySortInfo(d), "Массив не отсортирован")
 
 /**
  * ПОДСКАЗКИ
