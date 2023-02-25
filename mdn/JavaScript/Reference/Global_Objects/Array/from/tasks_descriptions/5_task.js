e08ac7480e75c39022277c3ecbd38315f64f3ecc
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-25 06:46:52 +0300

    add promise tasks

--- mdn/JavaScript/Reference/Global_Objects/Array/from/tasks_descriptions/5_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/from/tasks_descriptions/5_task.js
@@ -1,14 +1,12 @@
 /**
  * #Task 5: Сделайте массив из массивоподобного объекта (arguments)
  */
+import assert from "assert";
 
 function f() {
-  Array.from(arguments);
-  //TODO write code here
+  Array.from(arguments); //TODO write code here
 }
 
-f(1, 2, 3);
-// [1, 2, 3]
-
+assert.deepEqual(f(1, 2, 3), [1, 2, 3])
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/from#%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2_%D0%B8%D0%B7_%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D0%BE%D0%BF%D0%BE%D0%B4%D0%BE%D0%B1%D0%BD%D0%BE%D0%B3%D0%BE_%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%B0_arguments
