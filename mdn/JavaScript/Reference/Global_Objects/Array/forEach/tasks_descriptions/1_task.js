429c358c81ae333411a78c4e633cb751f387ce12
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-25 06:48:38 +0300

    add promise tasks

--- mdn/JavaScript/Reference/Global_Objects/Array/forEach/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/forEach/tasks_descriptions/1_task.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 /**
  * #Task 1: Сконвертируйте цикл for в forEach
  */
@@ -10,6 +11,6 @@ const copy = []
 // }
 
 //TODO write code here
-console.log(copy)
+assert.deepEqual(copy, ['item1', 'item2', 'item3'])
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach#%D0%BF%D0%B5%D1%87%D0%B0%D1%82%D1%8C_%D1%81%D0%BE%D0%B4%D0%B5%D1%80%D0%B6%D0%B8%D0%BC%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D0%B0
