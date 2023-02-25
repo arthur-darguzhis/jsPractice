594ecb03f1e48a3774ec81f624fbf0fe2a7c1db2
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-25 06:43:34 +0300

    add promise tasks

--- mdn/JavaScript/Reference/Global_Objects/Array/flat/tasks_descriptions/3_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/flat/tasks_descriptions/3_task.js
@@ -1,8 +1,10 @@
+import assert from "assert";
 /**
  * #Task 3: Преобразуйте массив [1, 2, [3, 4, [5, 6]]] в [1, 2, 3, 4, 5, 6]
  */
 
 const arr = [1, 2, [3, 4, [5, 6]]];
-console.log( /** TODO write code here */);
+const updatedArr = arr //TODO write code here
+assert(updatedArr, [1, 2, 3, 4, 5, 6])
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/flat#%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D1%8B
