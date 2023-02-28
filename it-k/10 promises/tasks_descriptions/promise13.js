3d63f5439cf3b6856e33a490626dca15ec48586c
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-28 08:58:37 +0300

    add promise tasks

---
+++ it-k/10 promises/tasks_descriptions/promise13.js
@@ -0,0 +1,29 @@
+/* Реализуйте промисификацию функции delay, что бы задавать таймер */
+
+setTimeout(() => {
+  console.log(1);
+  setTimeout(() => {
+    console.log(2);
+    setTimeout(() => {
+      console.log(3);
+    }, 3000);
+  }, 2000);
+}, 1000);
+
+const delay = (ms) => {
+  //TODO write code here
+};
+
+delay(1000)
+  .then((delayTime1) => console.log(delayTime1))
+  .then(() => delay(2000))
+  .then((delayTime2) => console.log(delayTime2))
+  .then(() => delay(3000))
+  .then((delayTime3) => console.log(delayTime3));
+
+const delayTime1 = await delay(1000);
+console.log(delayTime1);
+const delayTime2 = await delay(2000);
+console.log(delayTime2);
+const delayTime3 = await delay(3000);
+console.log(delayTime3);
