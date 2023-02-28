2de59512bf099629b46525ea99d96738db8a4762
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-03-01 01:11:48 +0300

    add promise tasks

--- 80tasks/backupOfTasks/65-ternary-operator/start.js
+++ 80tasks/backupOfTasks/65-ternary-operator/start.js
@@ -1,9 +1,11 @@
+import assert from "assert";
 /** ЗАДАЧА 65 - Тернарный оператор
  *
  * Измените функцию "isNumber", используя тернарный оператор
  */
 
 function isNumber(a) {
+  //TODO write code here
   if (typeof a === 'number') {
     return `${a} - это число`
   } else {
@@ -11,11 +13,6 @@ function isNumber(a) {
   }
 }
 
-console.log(isNumber(10))
-// 10 - это число
-
-console.log(isNumber('Привет'))
-// Привет - это не число
-
-console.log(isNumber(true))
-// true - это не число
+assert.equal(isNumber(10), '10 - это число')
+assert.equal(isNumber('Привет'), 'Привет - это не число')
+assert.equal(isNumber(true), 'true - это не число')
