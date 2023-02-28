fd83862a00210fc7ef8b8cbaf275dfde238d60ee
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-03-01 02:30:42 +0300

    add promise tasks

--- mdn/JavaScript/Reference/Global_Objects/Array/flat/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/flat/tasks_descriptions/1_task.js
@@ -1,8 +1,10 @@
+import assert from "assert";
 /**
  * #Task 1: преобразуйте массив [1, 2, [3, 4]] в [1, 2, 3, 4]
  */
 
 const arr = [1, 2, [3, 4]];
-console.log( /** TODO write code here */);
+const result = arr;
+assert.deepEqual(result, [1, 2, 3, 4]);
 
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/flat#%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D1%8B
