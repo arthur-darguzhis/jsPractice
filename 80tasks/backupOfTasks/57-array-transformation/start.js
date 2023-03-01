4d53ee58a0cda6a5a02a19baa955cdbd7b0fdb3a
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-03-01 19:51:12 +0300

    add promise tasks

--- 80tasks/backupOfTasks/57-array-transformation/start.js
+++ 80tasks/backupOfTasks/57-array-transformation/start.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 /** ЗАДАЧА 57 - Трансформация массива объектов
  *
  * 1. Создайте функцию "processPosts", которая будет возвращать новый массив сообщений
@@ -12,41 +13,54 @@
 const testPosts = [
   {
     postId: 234,
-    author: 'robd',
-    commentsQty: 5,
+    author: "robd",
+    commentsQty: 5
   },
   {
     postId: 823,
-    author: 'sady',
+    author: "sady"
   },
   {
     postId: 161,
-    author: 'merryl',
-    commentsQty: 8,
-  },
-]
-
-const processedPosts = processPosts(testPosts)
-console.log(processedPosts)
-/*
-[
-  {
-    postId: 1234,
-    postAuthor: 'robd',
-    postCommentsQty: 5
-  },
-  {
-    postId: 1823,
-    postAuthor: 'sady',
-    postCommentsQty: 0
-  },
-  {
-    postId: 1161,
-    postAuthor: 'merryl',
-    postCommentsQty: 8
+    author: "merryl",
+    commentsQty: 8
   }
-]
-*/
+];
 
-console.log(testPosts)
-// оригинальный массив должен остаться без изменений
+const processPosts = (sourceArr) => {
+  //TODO write code here
+};
+
+assert.deepEqual(processPosts(testPosts), [
+    {
+      postId: 1234,
+      postAuthor: "robd",
+      postCommentsQty: 5
+    },
+    {
+      postId: 1823,
+      postAuthor: "sady",
+      postCommentsQty: 0
+    },
+    {
+      postId: 1161,
+      postAuthor: "merryl",
+      postCommentsQty: 8
+    }
+  ]);
+assert.deepEqual(testPosts, [
+  {
+    postId: 234,
+    author: "robd",
+    commentsQty: 5
+  },
+  {
+    postId: 823,
+    author: "sady"
+  },
+  {
+    postId: 161,
+    author: "merryl",
+    commentsQty: 8
+  }
+]);
