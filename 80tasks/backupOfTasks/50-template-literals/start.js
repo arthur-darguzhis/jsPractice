0dc98a50aeb476f3a9f776ab31ffd0c932f2ec8d
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-03-01 20:12:01 +0300

    add promise tasks

--- 80tasks/backupOfTasks/50-template-literals/start.js
+++ 80tasks/backupOfTasks/50-template-literals/start.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 /** ЗАДАЧА 50 - Шаблонные строки
  *
  * 1. Создайте функцию "templateLiteral" с одним параметром "num"
@@ -7,20 +8,20 @@
  * 3. Ниже приведены примеры вызовов функции.
  */
 
+const templateLiteral = (num) => {
+  return `Число ${num}.
+Это число ${num < 10 ? "меньше" : "больше или равно"} 10.
+Квадратный корень этого числа - ${Math.sqrt(num)}.`;
+};
+
 // ТЕСТ 1
-const myNumber = 9
-console.log(templateLiteral(myNumber))
-/* 
-Число 9.
+const myNumber = 9;
+assert.equal(templateLiteral(myNumber), `Число 9.
 Это число меньше 10.
-Квадратный корень этого числа - 3.
-*/
+Квадратный корень этого числа - 3.`);
 
 // ТЕСТ 2
-const myAnotherNumber = 25
-console.log(templateLiteral(myAnotherNumber))
-/* 
-Число 25.
+const myAnotherNumber = 25;
+assert.equal(templateLiteral(myAnotherNumber), `Число 25.
 Это число больше или равно 10.
-Квадратный корень этого числа - 5.
-*/
+Квадратный корень этого числа - 5.`);
--- 80tasks/backupOfTasks/71-spread-operator/start.js
+++ 80tasks/backupOfTasks/71-spread-operator/start.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 /** ЗАДАЧА 71 - Spread Operator
  *
  * Используя оператор "spread" создайте новый массив
@@ -9,7 +10,7 @@ const b = [4, 5]
 const c = [8, 9, 10]
 const d = 11
 
-// Напишите код здесь
+const combinedArray = []//TODO write code here
 
-console.log(combinedArray)
-// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+assert(combinedArray, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+
