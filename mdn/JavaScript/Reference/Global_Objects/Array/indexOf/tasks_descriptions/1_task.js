c00c36902827eeee70a510611efe12e011a612d6
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-03-06 13:30:49 +0300

    add promise tasks

--- mdn/JavaScript/Reference/Global_Objects/Array/indexOf/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/indexOf/tasks_descriptions/1_task.js
@@ -2,7 +2,9 @@
  * #Task 1: Верните первый возможный индекс элемента 4
  */
 
-[1, 2, 3, 4, 5, 1, 2, 3, 4, 5] //TODO write code here
-//true
+import assert from "assert";
+
+const result = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5] //TODO write code here
+assert.equal(result, 3)
 
 // # На случаи затруднений: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
