e51eb5a6fdb28f56ed1c957312eb4e0d5c836145
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-05-29 12:05:46 +0300

    add assert condition

--- mdn/JavaScript/Reference/Global_Objects/Array/copyWithin/tasks_descriptions/1_task.js
+++ mdn/JavaScript/Reference/Global_Objects/Array/copyWithin/tasks_descriptions/1_task.js
@@ -1,3 +1,5 @@
+import assert from "assert";
+
 /**
  * #Task 1: Измените массив используя функцию Array.copyWithin так
  * что бы в результате содержимое стало [3, 4, 5, 4, 5]
@@ -9,6 +11,6 @@
 const arr = [1, 2, 3, 4, 5];
 arr.copyWithin(/* //TODO code here */);
 
-console.log(JSON.stringify(arr) === JSON.stringify([3, 4, 5, 4, 5]));
 
+assert.deepEqual(arr, [3, 4, 5, 4, 5])
 // # На случаи затруднений: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/copyWithin#%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D1%8B
